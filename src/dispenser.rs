6e48679f4283137b64ad227bb7e99cf7b78152b1
Vincent Chu <vincentchu3@gmail.com>
2025-07-05 06:39:11 +0000

    Axum bootstrap

---
+++ src/dispenser.rs
@@ -0,0 +1,9 @@
+use log::{info};
+
+pub fn dispense() -> Result<(), String> {
+    // Logic to dispense a treat
+    // This is a placeholder for the actual dispensing logic
+    // For example, it could interact with hardware to dispense a treat
+    info!("Treat dispensed!");
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+mod dispenser;
+
+use axum::{Router, response::IntoResponse, routing::get};
+use env_logger::Env;
+use log::{info, error};
+
+#[tokio::main]
+async fn main() {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info"))
+        .target(env_logger::Target::Stdout)
+        .init();
+    info!("Logger initialized");
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/dispense", get(dispense_treat));
+
+    // run our app with hyper, listening globally on port 3500
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3500").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> impl IntoResponse {
+    "Treat dispenser is online!"
+}
+
+
+async fn dispense_treat() -> impl IntoResponse {
+    // Logic to dispense a treat
+    match dispenser::dispense() {
+        Ok(_) => "Treat dispensed successfully!",
+        Err(e) => {
+            error!("Failed to dispense treat: {}", e);
+            "Failed to dispense treat."
+        }
+    }
 }
